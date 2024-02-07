072714efdec6311e3ddc22a2ac055c8bc28fd76a
Hung Ngo <htn002@bucknell.edu>
2024-02-07 14:02:51 -0500

    Set up PeerJS channels

--- desktop/public/script.js
+++ desktop/public/script.js
@@ -1,6 +1,12 @@
 const socket = io('/');
+const myPeer = new Peer(undefined, {
+    host: '/',
+    port: '8001'
+})
 
-socket.emit('join-room', MEETING_ID, 10);
+myPeer.on('open', id => {
+    socket.emit('join-room', MEETING_ID, id);
+})
 
 socket.on('user-connected', userId => {
     console.log("User connected: " + userId);
