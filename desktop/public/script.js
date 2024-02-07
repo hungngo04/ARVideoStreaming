059e0d63aa3fcfa24cedd12cee21c2476aa8241e
Hung Ngo <htn002@bucknell.edu>
2024-02-07 14:18:09 -0500

    Show webcam in the screen

--- desktop/public/script.js
+++ desktop/public/script.js
@@ -1,7 +1,18 @@
 const socket = io('/');
+const webcamVideo = document.getElementById('webcam-video');
 const myPeer = new Peer(undefined, {
     host: '/',
     port: '8001'
+})
+
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    addVideoStream(myVideo, stream);
 })
 
 myPeer.on('open', id => {
@@ -11,3 +22,11 @@ myPeer.on('open', id => {
 socket.on('user-connected', userId => {
     console.log("User connected: " + userId);
 })
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => { 
+        video.play();
+    });
+    webcamVideo.append(video);
+}
